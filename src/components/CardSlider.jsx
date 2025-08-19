fc9f777a0482a0ac2fc4f1b536d2474930535ab6
Joshuasam <joshuasam365@gmail.com>
2025-08-19 17:59:37 +0530

    Static to Public

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -20,11 +20,11 @@ export default function CardSlider() {
   return (
     <div style={{ width: '90%', margin: 'auto'}}>
     <Slider {...settings}>
-        <CourseCard  child={{ id: 0,image:"/static/images/card/react.png",title:"React Basics", info:"React is a JavaScript library for building user interfaces using reusable components. It uses a virtual DOM to efficiently update only the parts of the UI that change."}}/>
-        <CourseCard child={{id: 1,image:"/static/images/card/css.png",title:"CSS Mastery", info:"CSS (Cascading Style Sheets) is used to style HTML elements by controlling colors, fonts, and responsiveness. It separates content to create visually appealing web pages."}}/>
-        <CourseCard child={{id:2, image:"/static/images/card/js.png",title:"JavaScript Advanced", info:"JavaScript Advanced covers asynchronous programming, closures, prototypes, and ES6+ features to write more efficient code. It also includes concepts like promises, async/await."}}/>
-        <CourseCard child={{id:3,image:"/static/images/card/node.png",title:"Node.js", info:"Node.js is a JavaScript runtime built on Chrome’s V8 engine that allows you to run JS on the server. It enables building backend applications using event-driven, non-blocking I/O."}}/>
-        <CourseCard child={{id:4,image:"/static/images/card/ds.png",title:"Data Structures", info:"Data structures are ways to organize and store data for efficient access and modification. Common types include arrays, linked lists, stacks, queues, trees, and graphs."}}/>
+        <CourseCard  child={{ id: 0,image:"react.png",title:"React Basics", info:"React is a JavaScript library for building user interfaces using reusable components. It uses a virtual DOM to efficiently update only the parts of the UI that change."}}/>
+        <CourseCard child={{id: 1,image:"css.png",title:"CSS Mastery", info:"CSS (Cascading Style Sheets) is used to style HTML elements by controlling colors, fonts, and responsiveness. It separates content to create visually appealing web pages."}}/>
+        <CourseCard child={{id:2, image:"js.png",title:"JavaScript Advanced", info:"JavaScript Advanced covers asynchronous programming, closures, prototypes, and ES6+ features to write more efficient code. It also includes concepts like promises, async/await."}}/>
+        <CourseCard child={{id:3,image:"node.png",title:"Node.js", info:"Node.js is a JavaScript runtime built on Chrome’s V8 engine that allows you to run JS on the server. It enables building backend applications using event-driven, non-blocking I/O."}}/>
+        <CourseCard child={{id:4,image:"ds.png",title:"Data Structures", info:"Data structures are ways to organize and store data for efficient access and modification. Common types include arrays, linked lists, stacks, queues, trees, and graphs."}}/>
     </Slider>
     </div>
   )
