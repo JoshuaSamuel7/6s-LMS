a270c1aaad79940d4fd7fb45f6c4dbe9a98a1981
Joshuasam <joshuasam365@gmail.com>
2025-08-19 23:28:41 +0530

    slider debug

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -12,23 +12,24 @@ var settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
- responsive: [
+  initialSlide: 0,
+  responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToShow: 2,
       },
     },
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1,
+        initialSlide: 0,
       },
     },
   ],
 };
+
   return (
 <div style={{ width: '90%', maxWidth: '1200px', margin: 'auto' }}>
     <Slider {...settings}>
